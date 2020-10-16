cf42f82d09424f9900dd339bd5555748fcb58b01
f-chilmi <fchilmi5@gmail.com>
2020-10-16 21:33:27 +0700

    remove eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "airbnb", "airbnb/hooks"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"]
-    }
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+  ],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+  },
 };
--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
-import React, { Component } from 'react'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import {Provider} from 'react-redux'
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import store from './redux/store'
+import store from "./redux/store";
 
 // import components
-import PrivateRoute from './components/PrivateRoute'
+import PrivateRoute from "./components/PrivateRoute";
 
 // import pages
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import Cart from './pages/Cart'
-import Checkout from './pages/Checkout'
-import Address from './pages/Address'
-import Order from './pages/Order'
-import PageProduct from './pages/PageProduct'
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Address from "./pages/Address";
+import Order from "./pages/Order";
+import PageProduct from "./pages/PageProduct";
 
+// eslint-disable-next-line react/prefer-stateless-function
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <BrowserRouter>
           <Switch>
-            <Route path='/login' render={(props) => <Login {...props} /> } />
-            <Route path='/signup' render={(props) => <Signup {...props} /> } />
-            <Route path='/' render={(props) => <Home {...props} /> } exact />
-            <Route path='/page-product/:id' render={(props) => <PageProduct {...props} /> } />
-            <Route path='/order' render={(props) => <Order {...props} /> } />
-            <PrivateRoute path='/'  >
-              <Route path='/profile' render={(props) => <Profile {...props} /> } />
-              <Route path='/address' render={(props) => <Address {...props} /> } />
+            <Route path="/login" render={(props) => <Login {...props} />} />
+            <Route path="/signup" render={(props) => <Signup {...props} />} />
+            <Route path="/" render={(props) => <Home {...props} />} exact />
+            <Route path="/page-product/:id" render={(props) => <PageProduct {...props} />} />
+            <Route path="/order" render={(props) => <Order {...props} />} />
+            <PrivateRoute path="/">
+              <Route path="/profile" render={(props) => <Profile {...props} />} />
+              <Route path="/address" render={(props) => <Address {...props} />} />
               {/* <Route path='/order' render={(props) => <Order {...props} /> } /> */}
-              <Route path='/bag' render={(props) => <Cart {...props} /> } />
-              <Route path='/checkout' render={(props) => <Checkout {...props} /> } />
+              <Route path="/bag" render={(props) => <Cart {...props} />} />
+              <Route path="/checkout" render={(props) => <Checkout {...props} />} />
             </PrivateRoute>
           </Switch>
         </BrowserRouter>
       </Provider>
-    )
+    );
   }
 }
