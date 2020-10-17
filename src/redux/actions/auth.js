847514460129c950ef4eda60fadfb4bc40a54a6a
f-chilmi <fchilmi5@gmail.com>
2020-10-17 19:42:48 +0700

    auth.js action

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -2,15 +2,17 @@ import {default as axios} from 'axios'
 import http from '../../helpers/http'
 import qs from 'querystring'
 
+const {REACT_APP_BACKEND_URL} = process.env
+
 export default {
   login: (data) => ({
     type: 'AUTH_USER',
-    payload: http().post('http://localhost:8080/auth/login',qs.stringify(data))
+    payload: http().post(`${REACT_APP_BACKEND_URL}/auth/login`,qs.stringify(data))
     // payload: axios.post('http://localhost:8080/auth/login', data)
   }),
   signup: (data) => ({
     type: 'SIGNUP',
-    payload: http().post('http://localhost:8080/auth/signup',qs.stringify(data))
+    payload: http().post(`${REACT_APP_BACKEND_URL}/auth/signup`,qs.stringify(data))
   }),
   logout: () => ({
     type: 'LOGOUT'
